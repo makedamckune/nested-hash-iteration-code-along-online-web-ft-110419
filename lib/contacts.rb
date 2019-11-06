f35ffdf1714098cdd53f512e79adc04fbaf9a006
Makeda P Mckune <makedamckune@gmail.com>
2019-11-06 02:38:50 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -11,3 +11,12 @@ contacts = {
   }
 }
 
+contacts.each do |person, contact_details_hash|
+  if person == "Freddy Mercury"
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        binding.pry
+      end
+    end
+  end
+end
